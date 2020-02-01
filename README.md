df6041f9d82dff5bf015a051797dba9e1cc2b7fe
Alix <fullertonalix0@gmail.com>
2020-02-01 00:53:52 -0700

    Working on install process

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # LAN Nanny
-LAN Nanny is a tool for local network device discovery and monitoring. Lots of routers say they offer this kind of functionality, but it's often wrong, broken, slow or all of these. LAN Nanny provides the user with a secured web app to visualize all devices on a network, as well as open ports.
+LAN Nanny is a tool for local network device/port discovery and monitoring. Lots of routers
+masquerade users into thinking they have this functionality, but its broken, slow or wrong.
+LAN Nanny attempts to provide the user with a local, secure, web app to visualize all devices on a 
+network, as well as give a user a quick glance to a full networks worth of actively open ports, 
+either by port number, type or device currently advertising the exposed port.
 
 ## Install
-Coming soon
+```console
+git clone git@github.com:politeauthority/lan-nanny.git
+cd lan-nanny/
+python3 install-upgrade.py
+```
--- lan_nanny/install-upgrade.py
+++ lan_nanny/install-upgrade.py
@@ -2,11 +2,12 @@
 Creates the lan nanny db and tables, as well as populating options and their defaults.
 
 """
+import os
 import subprocess
 
 from modules import db
 from config import default as config_default
-conn, cursor = db.create_connection(config_default.LAN_NANNY_DB_FILE)
+# conn, cursor = db.create_connection(config_default.LAN_NANNY_DB_FILE)
 
 
 def run():
@@ -14,8 +15,38 @@ def run():
     Main entry point to scanning script.
 
     """
+    create_lan_nanny_space()
+    install_lan_nanny_python()
+    pip_requirements()
+    conn, cursor = create_database()
     db.create_tables(conn, cursor)
     db.populate_options(conn, cursor)
+
+def create_lan_nanny_space():
+    """Create the lan nanny install space"""
+    linux_install_path = "/opt/lan-nanny"
+
+    if not os.path.exists(linux_install_path):
+        os.mkdir(linux_install_path)
+    return True
+
+
+def create_database():
+    """Create the Lan Nanny DB, or establish connection, maybe both?"""
+    conn, cursor = db.create_connection(config_default.LAN_NANNY_DB_FILE)
+    return conn, cursor
+
+
+def install_lan_nanny_python():
+    """Run the python build/install process for lan nanny"""
+    setup_path = os.path.normpath(
+        os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), 
+            '../',
+            'setup.py'))
+    subprocess.Popen(['python3', setup_path, 'build'], True)
+    subprocess.Popen(['sudo', 'python3', setup_path, 'install'], True)
+    return True
 
 
 def pip_requirements():
@@ -23,10 +54,15 @@ def pip_requirements():
     Installs pip requirements.
 
     """
+    requirements_path = os.path.normpath(
+        os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), 
+            '../',
+            'requirements.txt'))
     try:
-        subprocess.check_output('pip3 install -r requirements.txt', shell=True)
+        subprocess.check_output('pip3 install -r %s' % requirements_path, shell=True)
     except subprocess.CalledProcessError:
-        subprocess.check_output('pip install -r requirements.txt', shell=True)
+        subprocess.check_output('pip install -r %s.txt' % requirements_path, shell=True)
     except subprocess.CalledProcessError:
         print('Error install pip requirements')
         exit(1)
